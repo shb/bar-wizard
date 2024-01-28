fbb1b74153da218052bc0237ebab7f920481fdff
Simone Cociancich <scociancich@cuebiq.com>
2024-01-28 18:07:31 +0100

    Add basic mixer editing

--- modules/wizard.js
+++ modules/wizard.js
@@ -1,5 +1,105 @@
 const canvas = Snap.select("#canvas")
 
+const diagram = {
+  glass: null,
+  mixers: null,
+
+  loadGlass(url) {
+    this.glass = null
+    canvas.children().forEach((child) => child.remove())
+    Snap.load(url, (template) => {
+      const glass = template.select("svg")
+      canvas.append(glass)
+      this.glass = glass
+      this.mixers = diagram.glass.select("#mixers")
+
+      this.updateMixers(recipe.mixers)
+    })
+  },
+
+  updateMixers(mixlist) {
+    this.mixers.clear()
+
+    const { x, y, w, h } = this.glass.select("#inner").getBBox()
+
+    let tv = 0
+    mixlist.forEach((mix, i) => {
+      const mv = h * 0.9 * mix.vol
+      tv += mv
+      this.mixers.rect(x, y + h - tv, w, mv).attr({
+        ["data-mixer"]: i,
+        fill: mix.fill,
+        fillOpacity: 1,
+      })
+    })
+  },
+}
+
+const recipe = {
+  _mixers: [],
+  _tot: 0,
+
+  get mixers() {
+    return this._mixers.map((mix) => ({
+      ...mix,
+      vol: mix.qty / this._tot,
+    }))
+  },
+
+  addMixer(def) {
+    this._mixers.push(def)
+    this._tot += Number(def.qty ?? 1)
+
+    this.selectMixer(this._mixers.length-1)
+
+    diagram.updateMixers(this.mixers)
+  },
+
+  selectMixer (i) {
+    state.mixer = i
+    const mixer = recipe.mixers[state.mixer]
+    
+    Object.entries(mixer).forEach(([name, value]) => {
+      const input = document.forms["recipe"].elements[name]
+      if (input) input.value = value
+    })
+  },
+
+  editMixer(i, update) {
+    this._tot -= Number(this._mixers[i].qty)
+    Object.assign(this._mixers[i], update)
+    this._tot += Number(this._mixers[i].qty)
+
+    diagram.updateMixers(this.mixers)
+  },
+
+  removeMixer(i) {
+    this._tot -= this.mixers[i]
+    this._mixers.splice(i)
+
+    diagram.updateMixers(this.mixers)
+  },
+}
+
+const state = {
+  pouring: false,
+  mixer: null,
+  step: null,
+}
+
+canvas.node.addEventListener("click", (e) => {
+  state.mixer = e.target.dataset["mixer"]
+  if (!state.mixer) return
+
+  e.stopPropagation()
+
+  recipe.selectMixer(state.mixer)
+})
+
+document.forms["recipe"].addEventListener("change", (e) => {
+  recipe.editMixer(state.mixer, { [e.target.name]: e.target.value })
+})
+
 pages.addEventListener("dragenter", onDrag)
 pages.addEventListener("dragover", onDrag)
 pages.addEventListener("drop", onDrop)
@@ -10,34 +110,49 @@ document.querySelectorAll(".page").forEach((page, i) => {
   })
 })
 
+//diagram.loadGlass("glasses/tumbler.svg")
 selectPage(0)
 
 function onDrag(e) {
+  switch (state.step) {
+    case "glasses":
+      e.dataTransfer.dropEffect = "move"
+      break
+    case "mixers":
+      if (!diagram.glass) return
+      e.dataTransfer.dropEffect = "copy"
+      //startPouring()
+      break
+    default:
+      return
+  }
   e.preventDefault()
-  e.dataTransfer.dropEffect = "move"
 }
 
 function onDrop(e) {
+  switch (state.step) {
+    case "glasses":
+      diagram.loadGlass(e.dataTransfer.getData("url"))
+      break
+    case "mixers":
+      if (!diagram.glass) return
+      addMixer()
+      break
+    default:
+      return
+  }
   e.preventDefault()
-  const url = e.dataTransfer.getData("url")
-  canvas.children().forEach((child) => child.remove())
-  loadGlass(url)
 }
 
-function loadGlass(url) {
-  Snap.load(url, (template) => {
-    const glass = template.select("svg")
-    canvas.append(glass)
-    render(glass)
-  })
-}
+function addMixer() {
+  if (state.pouring) return
+  if (!diagram.glass) return
 
-function render(glass) {
-  /* const mixers = glass.select("#mixers")
-  const layer = glass.rect(0, 10, 100, 50).attr({
-    fill: "slategray",
+  recipe.addMixer({
+    name: "Customize",
+    fill: "#BB3366",
+    qty: 1,
   })
-  mixers.append(layer) */
 }
 
 function selectPage(activeIndex) {
@@ -65,8 +180,9 @@ function selectPage(activeIndex) {
 }
 
 function selectStep(step) {
+  state.step = step
   document.querySelectorAll("body > aside > *").forEach((toolbar) => {
-    const steps = toolbar.dataset["step"]?.split(',') ?? []
+    const steps = toolbar.dataset["step"]?.split(",") ?? []
     toolbar.style.display = steps.includes(step) ? "block" : "none"
   })
 }
